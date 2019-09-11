038d09bcc728590c05ce4496f9c5b72094a0601d
manishunny <manish.swarnakar15@gmail.com>
2019-09-11 15:43:50 +0530

    Update PythonApplication18.py

--- PythonApplication18.py
+++ PythonApplication18.py
@@ -1,17 +1,60 @@
-#palimdrome using stack
+#All traversals
 
-_input=input("Enter the input?")
-data=[]
-stack=[]
+class node:
+    def __init__(self,val):
+        self.val=val
+        self.right=None
+        self.left=None
 
-for i in _input:
-    data.append(i)
-while(data):
-    stack.append(data.pop())
+def insert(root,node):
+    if root is None:
+        root=node
+    elif(root.val<node.val):
+        if root.right is None:
+            root.right=node
+        else:
+            insert(root.right,node)
+    else:
+        if root.left is None:
+            root.left=node
+        else:
+            insert(root.left,node)
 
-stack=("".join(stack))#convert list to string
 
-if(_input==stack):
-    print("It's a palindrome..!")
-else:
-    print("It's not a palindrome..!")
+r=input("Enter the root value- ")
+root=node(r)
+_node=int(input("How many nodes you want to enter- "))
+while(_node):
+    value=input("Enter the value- ")
+    insert(root,node(value))
+    _node-=1
+
+
+def inorder(root):
+    if root:
+        inorder(root.left)
+        print(root.val)
+        inorder(root.right)
+
+def preorder(root):
+    if root:
+        print(root.val)
+        preorder(root.left)
+        preorder(root.right)
+
+def postorder(root):
+    if root:
+        postorder(root.left)
+        postorder(root.right)
+        print(root.val)
+
+
+
+print("!.. Inorder Traversal..!")
+inorder(root)
+print("!.. Pre order Traversal..!")
+preorder(root)
+print("!.. Post order Traversal..!")
+postorder(root)
+
+        
